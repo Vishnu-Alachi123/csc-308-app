ba4051239c9a5fb6e6583184b04591410d04f41f
Vishnu Alachi <vishnualachi@gmail.com>
2025-09-29 11:44:38 -0700

    adding addition REST routes and findbyNameJob

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -45,17 +45,73 @@ const findUserByName = (name) => {
       (user) => user["name"] === name
     );
   };
+
+const findUserByNameJob = (name,job) => {
+return users["users_list"].filter(
+    (user) => user["name"] === name && user["job"] === job
+);
+};
   
-  app.get("/users", (req, res) => {
-    const name = req.query.name;
-    if (name != undefined) {
-      let result = findUserByName(name);
-      result = { users_list: result };
-      res.send(result);
-    } else {
-      res.send(users);
+const findUserById = (id) =>
+users["users_list"].find((user) => user["id"] === id);
+
+const removeUserById = (id) => {
+    const initialUserLength = users["users_list"].length;
+    users["users_list"] = users["users_list"].filter((user) => user["id"] != id)
+    const newUserLength = users["users_list"].length
+    return initialUserLength > newUserLength;
+}
+  
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+};
+
+app.get("/users", (req, res) => {
+const name = req.query.name;
+const job = req.query.job;
+if (name != undefined) {
+    if (job != undefined) {
+        let result = findUserByNameJob(name, job);
+        result = {user_list: result};
+        res.send(result);
     }
-  });
+    else{
+        let result = findUserByName(name);
+        result = { users_list: result };
+        res.send(result);
+    }
+} else {
+    res.send(users);
+}
+});
+
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send("Resource not found.");
+    } else {
+        res.send(result);
+    }
+});
+
+app.delete("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = removeUserById(id);
+    if (result === undefined) {
+        res.status(404).send("Resource not found.");
+    } else {
+        res.send(result);
+    }
+});
+
+app.post("/users", (req, res) => {
+const userToAdd = req.body;
+addUser(userToAdd);
+res.send();
+});
 
 app.listen(port, () => {
   console.log(
